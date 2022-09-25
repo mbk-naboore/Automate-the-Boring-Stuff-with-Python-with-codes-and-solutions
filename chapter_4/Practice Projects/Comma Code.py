493ea9e2879428ae2fe1159f8989c553565fb67a
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-25 23:28:57 +0300

    Update Comma Code.py

--- chapter_4/Practice Projects/Comma Code.py
+++ chapter_4/Practice Projects/Comma Code.py
@@ -1,1 +1,16 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+
+def comma_code(the_list):
+    the_full_sentence = str()
+    for i in the_list:
+        if i == the_list[-1]:
+            the_full_sentence += f"and {i}"
+            return the_full_sentence
+        else:
+            the_full_sentence += f"{i}, "
+    return the_full_sentence
+
+
+print(comma_code(spam))
 
